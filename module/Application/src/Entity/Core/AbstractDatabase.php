19f8a32747bfa238b9803b44f43630c9b6fd5f8d
Umarley Ricardo <umarleyricardo@gmail.com>
2021-11-08 13:00:40 -0300

    adição do include once na conexão com o banco de dados

--- module/Application/src/Entity/Core/AbstractDatabase.php
+++ module/Application/src/Entity/Core/AbstractDatabase.php
@@ -196,7 +196,9 @@ class AbstractDatabase extends TableGateway {
           $config = new \Laminas\Config\Config(include realpath('config/autoload/global.php'));
           } */
         $config = new \Laminas\Config\Config(include_once realpath('config/autoload/local.php'));
-
+        var_dump($config);
+        
+        exit;
         $arConfig = [];
         foreach ($config->db->adapters->$configName as $key => $value) {
             $arConfig[$key] = $value;
