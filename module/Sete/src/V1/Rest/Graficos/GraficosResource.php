d4e6088931dfa895345c8af0725c90ea1f88d174
pedsanches <61986850+pedsanches@users.noreply.github.com>
2022-03-25 10:08:56 -0300

    Finalização da implementação de graficos de alunos

--- module/Sete/src/V1/Rest/Graficos/GraficosResource.php
+++ module/Sete/src/V1/Rest/Graficos/GraficosResource.php
@@ -2,9 +2,10 @@
 namespace Sete\V1\Rest\Graficos;
 
 use Laminas\ApiTools\ApiProblem\ApiProblem;
-use Laminas\ApiTools\Rest\AbstractResourceListener;
+use Sete\V1\API;
 
-class GraficosResource extends AbstractResourceListener
+
+class GraficosResource extends API
 {
     /**
      * Create a resource
@@ -45,11 +46,141 @@ class GraficosResource extends AbstractResourceListener
      * @param  mixed $id
      * @return ApiProblem|mixed
      */
-    public function fetch($id)
-    {
-        return new ApiProblem(405, 'The GET method has not been defined for individual resources');
+    public function fetch($id) {
+        $arParams = $this->getEvent()->getRouteMatch()->getParams();
+        $dbGlbMunicipios = new \Db\SetePG\GlbMunicipios();
+        $codigoCidade = $arParams['codigo_cidade'];
+        if (!isset($codigoCidade) || empty($codigoCidade)) {
+            $this->populaResposta(400, ['result' => false, 'messages' => "O parâmetro codigo_cidade deve ser informado!"], false);
+        } else if (!$dbGlbMunicipios->municipioExiste($codigoCidade)) {
+            $this->populaResposta(404, ['result' => false, 'messages' => "O municipio informado não existe!"], false);
+        } else if (!$this->usuarioPodeAcessarCidade($codigoCidade)) {
+            $this->populaResposta(403, ['result' => false, 'messages' => "Usuário sem permissão para acessar o municipio informado!"], false);
+        } else {
+            if(isset($arParams['graficos_id'])){
+                $rota = $arParams['graficos_id'];
+                switch ($rota) {
+                    case 'alunos':
+                        $arResult['elementos'][0] = $this->getInfosAlunosAtendimento($codigoCidade);
+                        array_push($arResult['elementos'], $this->getInfosAlunosEscola($codigoCidade));
+                        array_push($arResult['elementos'], $this->getInfosAlunosRota($codigoCidade));
+                        array_push($arResult['elementos'], $this->getInfosAlunosEscolaridade($codigoCidade));
+                        array_push($arResult['elementos'], $this->getInfosAlunosTurno($codigoCidade));
+                        array_push($arResult['elementos'], $this->getInfosAlunosResidencia($codigoCidade));
+                        array_push($arResult['elementos'], $this->getInfosAlunosCor($codigoCidade));
+                        array_push($arResult['elementos'], $this->getInfosAlunosSexo($codigoCidade));
+                        array_push($arResult['elementos'], $this->getInfosAlunosResponsavel($codigoCidade));
+                        break;
+                    case 'rotas':
+                        break;
+                    default:
+                        $arResult = ['result' => false, 'messages' => "Recurso não existe!"];
+                        break;
+                }
+                $this->populaResposta(count($arResult) >= 1 ? 200 : 404, $arResult, false);
+            }
+        }
     }
 
+    private function getInfosAlunosAtendimento($codigoCidade){
+        $modelGraficos = new GraficosModel();
+        $arDados = $modelGraficos->getDadosAlunos($codigoCidade);
+        $arIds['codigo_cidade'] = $codigoCidade;
+        $arResposta['nome'] = "Atendimento";
+        $arResposta['titulo'] = "Porcentagem de Alunos Atendidos (Cadastrados no Sistema)";
+        $arResposta['labels'] = ["Sem Rota Cadastrada", "Com Rota Cadastrada"];
+        $arResposta['values'] = [$arDados['alunos_sem_rota'],$arDados['alunos_com_rota']];
+        return $arResposta;
+    }
+
+    private function getInfosAlunosEscola($codigoCidade){
+        $modelGraficos = new GraficosModel();
+        $arDados = $modelGraficos->getDadosEscolas($codigoCidade);
+        $arIds['codigo_cidade'] = $codigoCidade;
+        $arResposta['nome'] = "Escolas";
+        $arResposta['titulo'] = "Numéro Médio de Alunos por Escola";
+        $arResposta['labels'] =  ["Numéro Médio de Alunos Transportados por Escola"];
+        $arResposta['values'] = [$arDados['alunos_transportados_escola']];
+        return $arResposta;
+    }
+
+    private function getInfosAlunosRota($codigoCidade){
+        $modelGraficos = new GraficosModel();
+        $arDados = $modelGraficos->getDadosRotas($codigoCidade);
+        $arIds['codigo_cidade'] = $codigoCidade;
+        $arResposta['nome'] = "Rotas";
+        $arResposta['titulo'] = "Numéro Médio de Alunos por Rota";
+        $arResposta['labels'] =  ["Numéro Médio de Alunos Transportados por Rota"];
+        $arResposta['values'] = [$arDados['alunos_por_rota']];
+        return $arResposta;
+    }
+
+    private function getInfosAlunosEscolaridade($codigoCidade){
+        $modelGraficos = new GraficosModel();
+        $arDados = $modelGraficos->getDadosEscolaridade($codigoCidade);
+        $arIds['codigo_cidade'] = $codigoCidade;
+        $arResposta['nome'] = "Nível de Escolaridade";
+        $arResposta['titulo'] = "Distribuição de Alunos por Nível de Escolaridade";
+        $arResposta['labels'] = $arDados['labels'];
+        $arResposta['values'] = $arDados['values'];
+        
+        return $arResposta;
+    }
+
+    private function getInfosAlunosTurno($codigoCidade){
+        $modelGraficos = new GraficosModel();
+        $arDados = $modelGraficos->getDadosTurno($codigoCidade);
+        $arIds['codigo_cidade'] = $codigoCidade;
+        $arResposta['nome'] = "Turno de Aula";
+        $arResposta['titulo'] = "Distribuição de Alunos por Turno de Ensino";
+        $arResposta['labels'] = $arDados['labels'];
+        $arResposta['values'] = $arDados['values'];
+        return $arResposta;
+    }
+
+    private function getInfosAlunosResidencia($codigoCidade){
+        $modelGraficos = new GraficosModel();
+        $arDados = $modelGraficos->getDadosResidiencia($codigoCidade);
+        $arIds['codigo_cidade'] = $codigoCidade;
+        $arResposta['nome'] = "Área de Residência";
+        $arResposta['titulo'] = "Porcentagem de Alunos por Localização";
+        $arResposta['labels'] = $arDados['labels'];
+        $arResposta['values'] = $arDados['values'];
+        return $arResposta;
+    }
+
+    private function getInfosAlunosCor($codigoCidade){
+        $modelGraficos = new GraficosModel();
+        $arDados = $modelGraficos->getDadosCor($codigoCidade);
+        $arIds['codigo_cidade'] = $codigoCidade;
+        $arResposta['nome'] = "Cor";
+        $arResposta['titulo'] = "Porcentagem de Alunos por Cor/Raça";
+        $arResposta['labels'] = $arDados['labels'];
+        $arResposta['values'] = $arDados['values'];
+        return $arResposta;
+    }
+
+    private function getInfosAlunosSexo($codigoCidade){
+        $modelGraficos = new GraficosModel();
+        $arDados = $modelGraficos->getDadosSexo($codigoCidade);
+        $arIds['codigo_cidade'] = $codigoCidade;
+        $arResposta['nome'] = "Sexo";
+        $arResposta['titulo'] = "Porcentagem de Alunos por Sexo";
+        $arResposta['labels'] = $arDados['labels'];
+        $arResposta['values'] = $arDados['values'];
+        return $arResposta;
+    }
+
+    private function getInfosAlunosResponsavel($codigoCidade){
+        $modelGraficos = new GraficosModel();
+        $arDados = $modelGraficos->getDadosResponsavel($codigoCidade);
+        $arIds['codigo_cidade'] = $codigoCidade;
+        $arResposta['nome'] = "Responsável";
+        $arResposta['titulo'] = "Porcentagem de Alunos por Categoria de Responsável";
+        $arResposta['labels'] = $arDados['labels'];
+        $arResposta['values'] = $arDados['values'];
+        return $arResposta;
+    }
     /**
      * Fetch all or a subset of resources
      *
@@ -58,8 +189,10 @@ class GraficosResource extends AbstractResourceListener
      */
     public function fetchAll($params = [])
     {
-        return new ApiProblem(405, 'The GET method has not been defined for collections');
+        return new ApiProblem(405, 'The PATCH method has not been defined for collections');
     }
+
+    
 
     /**
      * Patch (partial in-place update) a resource
