05c351f7a7a2d86732d11d355f1debdc75389b72
Umarley Ricardo <umarleyricardo@gmail.com>
2022-07-18 21:44:14 -0300

    implementação do docker

--- docker-assets/local.php
+++ docker-assets/local.php
@@ -2,7 +2,7 @@
 return [
     'db' => [
         'adapters' => [
-            'sete_postgres' => [
+            'sete_api' => [
                 'database' => 'sete',
                 'driver' => 'PDO_pgsql',
                 'hostname' => '192.168.0.104',
